bc00d666b49e1d9089060c33f4f464fb03d4d0fa
LasaleFamine <info@godev.space>
2022-12-29 15:49:01 +0100

    feat: added script copy to arduino for mac

---
+++ scripts/copy_to_arduino.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script copies the main .cpp file and headers into the Arduino documents folder
+
+# Ensure lualtek-cubecell dir inside Arduino libraries
+mkdir -p ~/Documents/Arduino/libraries/lualtek-cubecell
+
+cp src/LualtekCubecellLib.cpp ~/Documents/Arduino/libraries/lualtek-cubecell
+cp library.json ~/Documents/Arduino/libraries/lualtek-cubecell
+cp include/LualtekCubecellLib.h ~/Documents/Arduino/libraries/lualtek-cubecell
